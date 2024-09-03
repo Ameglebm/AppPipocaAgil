d2cb26206cd10f8e2d70e025e5f05eb1efafccb2
Warlley Rocha <warlleyrocha@icloud.com>
2024-09-03 20:53:50 -0300

    restruturação de pastas, construção da tela de feedback cadastro realizado e atualização da tela de login

--- meu-app/frontend/App.js
+++ meu-app/frontend/App.js
@@ -22,10 +22,10 @@ export default function Login() {
     <KeyboardAvoidingView className="flex-1 justify-center items-center mx-2">
       <View className="pb-[32px]">
         <Image className="w-[76px] h-[66px] shrink-0"
-        source={require('./assets/images/image.webp')}/>
+        source={require('./assets/images/user.webp')}/>
       </View>
 
-      <View className="flex-shrink-0 w-[350px] h-[300px] bg-[#EFEFF0] rounded-[20px] p-5">
+      <View className="flex-shrink-0 w-[350px] h-[385px] bg-[#EDF3FF] rounded-[16px] p-5">
 
         <View>
           <ToggleButton />
@@ -33,7 +33,7 @@ export default function Login() {
 
         <View>
           <View className="space-y-1">
-            <Text className="text-[14px] pb-3 text-[#AFB1B6]">E-mail</Text>
+            <Text className="text-[14px] pb-3 text-[#282828]">E-mail</Text>
             <TextInput 
             className="text-[16px] p-2 border-[1px] border-[#b7b7b8] rounded-md" placeholder="Email@correto.com" 
             onChangeText={value =>setEmail(value)}
@@ -43,38 +43,40 @@ export default function Login() {
           </View>
 
           <View className="space-y-1 pt-4">
-            <Text className="text-[14px] pb-3 text-[#AFB1B6]">Senha</Text>
+            <Text className="text-[14px] pb-3 text-[#282828]">Senha</Text>
             <ShowHide placeholder="Digite sua senha" onChangeText={text=>setPassword(text)} secureTextEntry={true} />
           </View>          
         </View>
 
+        <View className="pt-[10px] pb-[8px] px-3 flex-col inline-flex justify-center items-center gap-4">
+          <TouchableOpacity>
+            <Text className="text-[#2933AA] text-[14px] font-bold not-italic leading-[19.6px] absolute mt-[16px] right-[-165px]">Esqueceu a senha?</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex bg-[#2F39D3] p-4 mt-[58px] w-[320px] min-h-[42px] items-center rounded-2xl">
+          <TouchableOpacity>
+            <Text className="text-[#FDFDFD] text-[22px]">Entrar</Text>
+          </TouchableOpacity>
+        </View>
+
       </View>
 
-      <View className="pt-[10px] pb-[8px] px-3 flex-col inline-flex justify-center items-center gap-4">
-        <TouchableOpacity>
-          <Text className="text-[#3A00E5] text-[12px] font-medium not-italic leading-4 tracking-[0.6px] absolute top-[10px] right-[-165px]">Esqueceu a senha?</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View className="flex flex-col bg-[#969696] p-4 mt-[45px] w-[305px] min-w-[305px] items-center rounded-full">
-        <TouchableOpacity>
-          <Text className="text-white text-[22px]">Entrar</Text>
-        </TouchableOpacity>
-      </View>
+      
 
       <View className="flex gap-[16px] justify-center items-center m-[8px]">
-        <View className="flex flex-row">
-          <Text className="pt-[10px] text-[#969696]">Não possui uma conta? </Text>
+        <View className="flex flex-row text-[14px]">
+          <Text className="pt-[10px] text-[#464646]">Não possui uma conta? </Text>
           <TouchableOpacity>
-            <Text className="pt-[10px] text-[#3A00E5]">Cadastre-se!</Text>
+            <Text className="pt-[10px] text-[#2933AA] text-[14px] font-bold not-italic leading-[19.6px]">Cadastre-se!</Text>
           </TouchableOpacity>
         </View>
         
 
         <View className="flex flex-row">
-          <Text className="pt-[10px] text-[#969696]">Dúvidas? </Text>
+          <Text className="pt-[10px] text-[#464646] text-[14px]">Dúvidas? </Text>
           <TouchableOpacity>
-            <Text className="pt-[10px] text-[#3A00E5]">Fale com a gente</Text>
+            <Text className="pt-[10px] text-[#2933AA] text-[14px] font-bold not-italic leading-[19.6px]">Fale com a gente</Text>
           </TouchableOpacity>
         </View>
         
--- meu-app/frontend/components/toggleButton.jsx
+++ meu-app/frontend/components/toggleButton.jsx
@@ -11,14 +11,14 @@ const ToggleButton = () => {
           style={[styles.tab, activeTab === 'active' && styles.activeTab]}
           onPress={() => setActiveTab('active')}
         >
-          <Text style={styles.tabText}>Paciente</Text>
+          <Text style={styles.tabTextActive}>Paciente</Text>
         </TouchableOpacity>
         <View className="w-[1px] bg-[#AFB1B6]" />
         <TouchableOpacity
           style={[styles.tab, activeTab === 'inactive' && styles.activeTab]}
           onPress={() => setActiveTab('inactive')}
         >
-          <Text style={styles.tabText}>Médico</Text>
+          <Text style={styles.tabTextOff}>Médico</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -42,13 +42,16 @@ const styles = StyleSheet.create({
   tab: {
     flex: 1,
     padding: 10,
-    backgroundColor: '#EFEFF0',
+    backgroundColor: '#FDFDFD',
     alignItems: 'center',
   },
   activeTab: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#6179FA',
   },
-  tabText: {
+  tabTextActive: {
+    color: '#FDFDFD',
+  },
+  tabTextOff: {
     color: 'black',
   },
 });
