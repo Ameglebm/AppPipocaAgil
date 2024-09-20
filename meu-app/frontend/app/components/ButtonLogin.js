8a1602d1209209f37ce7e8d488206ac15f773035
Warlley Rocha <warlleyrocha@icloud.com>
2024-09-20 11:04:28 -0300

    correcoes no tela de login e componente button

--- meu-app/frontend/app/components/ButtonLogin.js
+++ meu-app/frontend/app/components/ButtonLogin.js
@@ -7,7 +7,7 @@ const ButtonLogin = ({ labelButton, onpress }) => {
   const router = useRouter();
    
     return(
-        <TouchableOpacity className="flex bg-[#2F39D3] shadow-3xl px-[80px] py-2 mt-[58px] w-[320px] min-h-[42px] justify-center items-center rounded-[8px]" onPress={() => router.push('../screens/checkSucess')}>
+        <TouchableOpacity className="flex justify-center items-center w-full min-h-[46px] px-[12px] mt-[18px] bg-[#2F39D3] shadow-3xl rounded-[8px]" onPress={() => router.push('../screens/checkSucess')}>
           <Text className="text-[#FDFDFD] text-[18px] font-bold leading-[19,80px]">{labelButton}</Text>
         </TouchableOpacity>
     )
--- meu-app/frontend/app/screens/login.jsx
+++ meu-app/frontend/app/screens/login.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect} from "react";
-import { KeyboardAvoidingView, TextInput, Text, View, Image, TouchableOpacity } from "react-native";
+import { KeyboardAvoidingView, View, SafeAreaView, TextInput, Text, Image, TouchableOpacity } from "react-native";
 import { Link, useRouter } from 'expo-router'
 import ShowHide from "../components/showHide";
 import ButtonLogin from "../components/ButtonLogin";
@@ -13,39 +13,37 @@ export default function Login() {
   const [errorPassword, setErrorPassword]= useState(null);
 
   //Enviar dados do formulário para o Backend
-  async function sendForm() {
-    let response = await fetch('/url-do-backend', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: email,
-        password: password
-      })
-    });
+ 
+
+ /* const validar = () => {
+    setErrorEmail("Prencha corretamente")
+    return false
+  }*/
+
+  const signIn = () => {
+    console.log('click');
   }
 
   return (
-    <KeyboardAvoidingView className="flex-1 justify-center items-center mx-2 bg-[#FDFDFD]">
-      <View className="pb-[32px]">
-        <Image className="w-[66px] h-[66px] top-[-100]"
+    <KeyboardAvoidingView className="bg-[#FDFDFD] flex items-center pt-[48px] w-full h-full">
+      <View>
+        <Image className="w-[66px] h-[66px]"
         source={require('../assets/images/user.webp')}/>
       </View>
 
-      <View className="flex-shrink-0 w-[400px] h-[322px] bg-[#EDF3FF] rounded-2xl p-5">
+      <View className="w-[392px] h-[300px] top-[32px] bg-[#EDF3FF] rounded-2xl">
 
-        {/* <View>              //Botão ocultado [MVP]
-          <ToggleButton />
-        </View> 
+        {/* Botão de alternância
+            <View>
+              <ToggleButton />
+            </View> 
         */}
 
-        <View>
-          <View className="space-y-1">
+        <View className="px-[12px] pt-[22px]">
+          <View className="">
             <Text className="text-[14px] pb-3 text-[#282828]">E-mail*</Text>
             <TextInput 
-            className="text-[16px] p-2 border-[1px] border-[#b7b7b8] bg-[#FDFDFD] rounded-md" placeholder="email@correto.com" 
+            className="text-[16px] p-2 border-[1px] border-[#b7b7b8] bg-[#FDFDFD] shadow-3xl rounded-md" placeholder="email@correto.com" 
             onChangeText={text => setEmail(text)}
             keyboardType="email-address"
             errorMessage={errorEmail}
@@ -53,31 +51,30 @@ export default function Login() {
           </View>
 
           <View className="space-y-1 pt-4">
-            <Text className="text-[14px] pb-3 text-[#282828]">Senha*</Text>
-            <ShowHide placeholder="Digite sua senha" onChangeText={text => setPassword(text)} secureTextEntry={true} />
+            <Text className="text-[14px] pb-3 text-[#282828]">Senha</Text>
+            <ShowHide placeholder="Digite sua senha" onChangeText={(text) => setPassword(text)} secureTextEntry={true} />
           </View>          
         </View>
-
-        <View className="pt-[10px] pb-[8px] px-3 flex-col inline-flex justify-center items-center gap-4">
+        
+        {/*Código corrigido */}
+        <View className="pt-[16px] pr-3 items-end">
           <TouchableOpacity>
-            <Text className="text-[#2933AA] text-[14px] font-bold not-italic leading-[19.6px] absolute mt-[16px] right-[-165px]">Esqueceu a senha?</Text>
+            <Text className="text-[#2933AA] text-[14px] font-bold not-italic leading-[19.6px]">Esqueceu a senha?</Text>
           </TouchableOpacity>
         </View>
 
-        <View className="flex-1 justify-center items-center">
-          <ButtonLogin labelButton="Entrar" onpress={sendForm}></ButtonLogin>
+        {/*Código corrigido */}
+        <View className="flex justify-center items-center px-[12px] shadow-3xl"> 
+          <ButtonLogin labelButton="Entrar"></ButtonLogin>
         </View>
 
       </View>
 
-      <View className="flex gap-[16px] justify-center items-center m-[8px]">
-        <View className="flex flex-row text-[14px]">
-          <Text className="pt-[10px] text-[#464646]">Não possui uma conta? </Text>
-          <Link href={"/cadastro"} className="pt-[10px] text-[#2933AA] text-[14px] font-bold not-italic leading-[19.6px]"> Cadastre-se!</Link>
-        </View>
-        
+      {/* Código corrigido */}   
+      <View className="flex-1 flex-row text-[14px] justify-end items-center pt-[24px]">  
+        <Text className="text-[#464646]">Não possui uma conta? </Text>
+        <Link href={"/cadastro"} className="text-[#2933AA] text-[14px] font-bold not-italic leading-[19.6px]"> Cadastre-se!</Link>
       </View>
-
     </KeyboardAvoidingView>
   );
 }
