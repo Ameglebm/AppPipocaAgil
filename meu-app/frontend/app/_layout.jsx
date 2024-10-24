e5aabc59fc36e8a3b409dcc146632059e445a7a0
Wesley Lopes dos Reis <lopeswesley1392@gmail.com>
2024-10-24 18:03:37 -0300

    tela de recuperacao de senha criada

--- meu-app/frontend/app/_layout.jsx
+++ meu-app/frontend/app/_layout.jsx
@@ -1,9 +1,30 @@
 import { Stack, useRouter } from "expo-router";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { TouchableOpacity } from "react-native";
+import { Urbanist_700Bold, useFonts } from "@expo-google-fonts/urbanist";
+import { Lato_400Regular } from "@expo-google-fonts/lato";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
   const router = useRouter();
+
+  const [loaded, error] = useFonts({
+    Urbanist_700Bold,
+    Lato_400Regular,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
 
   return (
     <Stack screenOptions={{}} /*config para todas as telas*/>
@@ -88,6 +109,14 @@ export default function Layout() {
               <AntDesign name="left" size={24} color="black" />
             </TouchableOpacity>
           ),
+        }}
+      />
+
+      {/* Tela de Recuperação de Senha */}
+      <Stack.Screen
+        name="recuperacaoSenha/index"
+        options={{
+          headerShown: false, // Sem header na tela de recuperação de senha
         }}
       />
 
