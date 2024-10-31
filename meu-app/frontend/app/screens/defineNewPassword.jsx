a5824fbe8966aa234bd7fe25923bee6433d1ca78
Warlley Rocha <warlleyrocha@icloud.com>
2024-10-30 22:17:05 -0300

    criacao das variaveis para setar o estado dos inputs, adicao da funcionalidade de visibilidade da senha e inclusao da sombra nos campos dos inputs

--- meu-app/frontend/app/screens/defineNewPassword.jsx
+++ meu-app/frontend/app/screens/defineNewPassword.jsx
@@ -1,12 +1,20 @@
+import React, {useEffect, useState} from 'react'
 import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native'
-import React, {useEffect} from 'react'
-import { useNavigation } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
 import { useFonts, Urbanist_700Bold } from "@expo-google-fonts/urbanist";
 import { Lato_400Regular} from "@expo-google-fonts/lato";
 
 export default function defineNewPassword() {
     const navigation = useNavigation();
+    
+    const [senha, setSenha] = useState("");
+    const [confirmarSenha, setConfirmarSenha] = useState("");
+    const [secureText, setSecureText] = useState(true);
+    const [secureText2, setSecureText2] = useState(true);
+    
+    const [errors, setErrors] = useState({});
 
     useEffect(() => { //Ao iniciar a página seta o header dela como false
         navigation.setOptions({headerShown: false});
@@ -17,9 +25,18 @@ export default function defineNewPassword() {
         Lato_400Regular,
       });
 
+      const handlePress = () => {
+        setIsChecked((prevState) => {
+          const newState = !prevState;
+          setButtonColor(newState ? "#2F39D3" : "#7A98FF");
+          setIsDisabled(!newState);
+          return newState;
+        });
+      };
+
   return (
     <SafeAreaProvider styles={{flex: 1}}>
-        <SafeAreaView>
+        <SafeAreaView style={styles.mainContainer}>
             <View style={styles.titleContainer}>
                 <Text style={styles.title}>Defina sua nova senha</Text>
             </View>
@@ -28,19 +45,59 @@ export default function defineNewPassword() {
                 <View style={styles.container}>
                     <View>
                         <Text style={styles.subTitle}>Senha*</Text>
-                        <TextInput style={styles.inputs} placeholder='Digite sua senha'></TextInput>
+                        <TextInput 
+                        style={styles.inputs} 
+                        placeholder='Digite sua senha'
+                        secureTextEntry={secureText}
+                        value={senha}
+                        onChangeText={setSenha}
+                        placeholderTextColor="#B1B0AF"
+                        />
+                        <Pressable
+                        style={styles.iconBtn}
+                        onPress={() => setSecureText((prevState) => !prevState)}
+                        >
+                            <Feather
+                              name={secureText ? "eye-off" : "eye"}
+                              size={17}
+                              color="#B1B0AF"
+                              style={styles.eyeIcon}
+                            />
+                        </Pressable>
                     </View>
 
                     <View>
                         <Text style={styles.subTitle}>Confirmar senha*</Text>
-                        <TextInput style={styles.inputs} placeholder='Confirme sua senha'></TextInput>
+                        <TextInput 
+                        style={styles.inputs} 
+                        placeholder='Confirme sua senha'
+                        secureTextEntry={secureText}
+                        value={senha}
+                        onChangeText={setSenha}
+                        placeholderTextColor="#B1B0AF"
+                        />
+                        <Pressable
+                        style={styles.iconBtn}
+                        onPress={() => setSecureText((prevState) => !prevState)}
+                        >
+                            <Feather
+                              name={secureText ? "eye-off" : "eye"}
+                              size={17}
+                              color="#B1B0AF"
+                              style={styles.eyeIcon}
+                            />
+                        </Pressable>
                     </View>
                 </View>
 
                 <Pressable style={styles.btn}>
-                <View style={styles.shadowLayer1}/>
-                <View style={styles.shadowLayer2}/>
                     <Text style={styles.textBtn}>Confirmar</Text>
+
+                    {/*
+                    <View style={styles.shadowLayer1}/>
+                    <View style={styles.shadowLayer2}/>
+                    */}
+
                 </Pressable>
             </View>
         </SafeAreaView>
@@ -50,7 +107,6 @@ export default function defineNewPassword() {
 
 const styles = StyleSheet.create({
     titleContainer: {
-        
         marginBottom: 64,
     },
     title:{
@@ -68,6 +124,10 @@ const styles = StyleSheet.create({
         alignItems: "flex-start",
         gap: 16,
     },
+    mainContainer: {
+        flex: 1,
+        backgroundColor: "#FDFDFD",
+    },
     subTitle: {
         fontFamily: "Lato_400Regular",
         fontSize: 14,
@@ -76,19 +136,22 @@ const styles = StyleSheet.create({
         marginBottom: 8,
     },
     inputs: {
+        backgroundColor: "#FDFDFD",
         fontFamily: "Lato_400Regular",
         fontSize: 16,
         fontStyle: "normal",
         lineHeight: 22,
-        marginBottom: 8,
         width: 320,
         height: 44,
         paddingVertical: 10,
         paddingHorizontal: 14,
         alignItems: "center",
+        alignSelf: "center",
         borderRadius: 8,
-        backgroundColor: "#FDFDFD",
-        zIndex: 1, // Certifique-se de que a caixa esteja acima das sombras
+        borderStyle: "solid",
+        borderColor: "#B1B0AF",
+        borderWidth: 0.8,
+        elevation: 5,
     },
     textBtn: {
         color: "#FDFDFD",
@@ -98,6 +161,7 @@ const styles = StyleSheet.create({
         lineHeight: 19.8,
     },
     btn: {
+        backgroundColor: "#2F39D3",
         width: 320,
         height: 42,
         paddingVertical: 8,
@@ -105,11 +169,23 @@ const styles = StyleSheet.create({
         marginTop: 32,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#2F39D3",
+        alignSelf: "center",
         borderRadius: 8,
-        zIndex: 1, // Certifique-se de que a caixa esteja acima das sombras
+        elevation: 10,
     },
-    shadowLayer1: {
+    iconBtn: {
+        position: "absolute",
+        top: 41,
+        right: 15,
+        zIndex: 1,
+      },
+      eyeIcon: {
+        width: 24,
+        height: 24,
+      },
+    
+      /*
+      shadowLayer1: {
         position: 'absolute',
         left: 0,
         top: 2,
@@ -165,4 +241,5 @@ const styles = StyleSheet.create({
         elevation: 5,
         borderRadius: 8,
     },
+    */
 })
