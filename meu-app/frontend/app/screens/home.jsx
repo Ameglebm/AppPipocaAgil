74780d4a33f4f79605341cbde1781470b35b1601
Warlley Rocha <warlleyrocha@icloud.com>
2024-11-04 20:27:43 -0300

    inicio de desenvolvimento HomeScreen

---
+++ meu-app/frontend/app/screens/home.jsx
@@ -0,0 +1,10 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function home() {
+  return (
+    <View>
+      <Text>home</Text>
+    </View>
+  )
+}
