59b5df6be97581c7beabf820848390343e0f9712
Warlley Rocha <warlleyrocha@icloud.com>
2024-11-04 10:59:44 -0300

    desenvolvimento das telas de feedback de acordo com o figma

--- meu-app/frontend/app/screens/recoverSucessfull.jsx
+++ meu-app/frontend/app/screens/recoverSucessfull.jsx
@@ -1,41 +1,79 @@
-import {Text, View, Image, TouchableOpacity} from "react-native"
-import React from "react";
-import { useNavigation } from "@react-navigation/native";
-import ButtonLogin from '../components/ButtonLogin';
+import React, { useEffect } from "react";
+import {Text, View, Image, TouchableOpacity, Button, StyleSheet} from "react-native"
+import { useNavigation, useRouter } from "expo-router";
+import { useFonts, Lato_400Regular  } from "@expo-google-fonts/lato";
+import { Urbanist_700Bold } from "@expo-google-fonts/urbanist";
 
-export default function recoverSucessfull() {
+export default function recoverAccountEmail() {
     const navigation = useNavigation();
+    const router = useRouter();
+
+    useEffect(() => {
+        //Ao iniciar a página seta o header dela como false
+        navigation.setOptions({ headerShown: false });
+    }, [navigation]);
+
+    const [fonteLoaded] = useFonts({
+        Lato_400Regular,
+        Urbanist_700Bold,
+    });
+
     return (
-        <View style={{ paddingTop: 20, paddingHorizontal: 16 }}>  
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <View style={{flex: 1}}>  
+            <View style={{marginTop: 46, paddingHorizontal: 20}}>
                 <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <Image
-                        style={{ width: 24, height: 24 }}
-                        source={require("../assets/images/backIcon.png")}
-                    />
+                    <Image style={{ width: 24, height: 24 }} source={require("../assets/images/backIcon.png")} />
                 </TouchableOpacity>
             </View>
 
-            <View>
-                <Image
-                    className="w-[168] h-[198.4]"
-                    source={require("../assets/images/undraw_mail_sent_re_0ofv1.png")}
-                />
-            </View>
+            <View style={styles.container}>
+                <View>
+                    <Image style={styles.image} source={require("../assets/images/undraw_confirmed_re_sef71.png")} />
+                </View>
 
-            <View className="mt-[32px]">
-                <Text className="text-[20px] text-[#373737] font-semibold not-italic leading-[22px]">
-                    Senha recuperada com sucesso!
-                </Text>
-            </View>
+                <View>
+                    <Text style={styles.text}>
+                        Senha recuperada com sucesso!
+                    </Text>
+                </View>
 
-            <ButtonLogin
-                labelButton="Login"
-                onpress={() => {navigation.navigate("./login")
-                }}
-            />
+                <TouchableOpacity style={styles.btn} onPress={router.push("./login")}>
+                    <Text style={styles.textBtn}>Login</Text>
+                </TouchableOpacity>
+            </View>
+            
         </View>
-
-        );
-    
+    );
 }
+    const styles = StyleSheet.create ({
+        container: {
+            flex: 0.9,
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 32,
+        },
+
+        text: {
+            fontFamily: "Urbanist_700Bold",
+            fontSize: 20,
+            fontStyle: "normal",
+            lineHeight: 22,
+            textAlign: "center",
+            color: "#373737",
+        },
+        btn: {
+            backgroundColor: "#2F39D3",
+            paddingHorizontal: 42,
+            paddingVertical: 8,
+            borderRadius: 8,
+            borderWidth: 1,
+            borderColor: '#2F39D3', // Persian Blue
+        },
+        textBtn: {
+            fontFamily: "Urbanist_700Bold",
+            fontSize: 18,
+            fontStyle: "normal",
+            lineHeight: 19.8,
+            color: "#FDFDFD",
+        }
+    })
