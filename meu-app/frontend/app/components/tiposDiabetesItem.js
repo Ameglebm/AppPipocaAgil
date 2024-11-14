4015f7f887459b0f3fb31199cb95f8370dabf94c
Warlley Rocha <warlleyrocha@icloud.com>
2024-11-13 22:29:30 -0300

    ajuste na largura de cada item para ocupar toda a tela

--- meu-app/frontend/app/components/tiposDiabetesItem.js
+++ meu-app/frontend/app/components/tiposDiabetesItem.js
@@ -1,10 +1,10 @@
-import { View, Text, StyleSheet, TouchableOpacity, FlatList, useWindowDimensions } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, useWindowDimensions } from 'react-native';
+import React, { useState } from 'react';
 
-export default tiposDiabetesItem = ({item}) => {
-    const { width } = useWindowDimensions();
-    const isTipoDiabetesScreen = item.id === '1';
-    const diabetesTypes = isTipoDiabetesScreen
+export default function TiposDiabetesItem({ item }) {
+  const { width } = useWindowDimensions();
+  const isTipoDiabetesScreen = item.id === '1';
+  const diabetesTypes = isTipoDiabetesScreen
     ? [
         item.typeOne,
         item.typeTwo,
@@ -17,24 +17,26 @@ export default tiposDiabetesItem = ({item}) => {
       ]
     : [];
 
-     // Estado para controlar o tipo selecionado
-    const [selectedType, setSelectedType] = useState(null);
+  // Estado para controlar o tipo selecionado
+  const [selectedType, setSelectedType] = useState(null);
 
-    // Função para selecionar o tipo de diabetes
-    const handleSelectType = (type) => {
-        setSelectedType(type);
-    };
+  // Função para selecionar o tipo de diabetes
+  const handleSelectType = (type) => {
+    setSelectedType(type);
+  };
+
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.title}>{item.title}</Text>
 
-        {isTipoDiabetesScreen && (
+      {isTipoDiabetesScreen && (
         <FlatList
           data={diabetesTypes}
           renderItem={({ item }) => (
             <TouchableOpacity
               style={[
                 styles.typeContainer,
+                { width: width - 40 }, // Ajusta a largura de cada item para ocupar toda a tela com margens
                 selectedType === item && styles.selectedTypeContainer,
               ]}
               onPress={() => handleSelectType(item)}
@@ -53,41 +55,41 @@ export default tiposDiabetesItem = ({item}) => {
         />
       )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      padding: 20,
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 10,
-    },
-    typeContainer: {
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        borderRadius: 30,
-        marginVertical: 5,
-        backgroundColor: '#FDFDFD',
-        // Configuração de sombra para iOS
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.05,
-        shadowRadius: 4,
+  container: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  typeContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 30,
+    marginVertical: 5,
+    backgroundColor: '#FDFDFD',
+    // Configuração de sombra para iOS
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.05,
+    shadowRadius: 4,
 
-        // Configuração de sombra para Android
-        elevation: 3, // Use valores baixos para uma sombra mais sutil
-      },
-      selectedTypeContainer: {
-        backgroundColor: '#7A98FF', // Cor de destaque para o item selecionado
-      },
-      typeText: {
-        fontSize: 18,
-        color: '#333',
-      },
-      selectedTypeText: {
-        color: '#FDFDFD', // Cor de destaque do texto selecionado
-      },
-  });
+    // Configuração de sombra para Android
+    elevation: 3, // Use valores baixos para uma sombra mais sutil
+  },
+  selectedTypeContainer: {
+    backgroundColor: '#7A98FF', // Cor de destaque para o item selecionado
+  },
+  typeText: {
+    fontSize: 18,
+    color: '#333',
+  },
+  selectedTypeText: {
+    color: '#FDFDFD', // Cor de destaque do texto selecionado
+  },
+});
