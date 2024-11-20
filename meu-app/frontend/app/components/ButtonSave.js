01ed512b44e99ea5b3c36acf34146925343618a4
Warlley Rocha <warlleyrocha@icloud.com>
2024-11-20 09:55:09 -0300

    alterado Pressable para Touchableopacity

--- meu-app/frontend/app/components/ButtonSave.js
+++ meu-app/frontend/app/components/ButtonSave.js
@@ -1,17 +1,12 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
-import { useFonts, Urbanist_700Bold } from "@expo-google-fonts/urbanist";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 
 export default function ButtonSave({ scrollTo }) {
-  const [fonteLoaded] = useFonts({
-    Urbanist_700Bold,
-  });
-
   return (
     <View style={styles.container}>
-      <Pressable onPress={scrollTo} style={styles.moveButtton}>
+      <TouchableOpacity onPress={scrollTo} style={styles.moveButtton}>
         <Text style={styles.textButton}>Salvar</Text>
-      </Pressable>
+      </TouchableOpacity>
     </View>
   );
 }
