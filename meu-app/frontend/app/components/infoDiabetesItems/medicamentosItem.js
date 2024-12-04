a7388fb847dfcba1851e8303479dcd821dd73666
Warlley Rocha <warlleyrocha@icloud.com>
2024-12-04 16:09:00 -0300

    feat: alterar rota apenas para acessar a tela

--- meu-app/frontend/app/components/infoDiabetesItems/medicamentosItem.js
+++ meu-app/frontend/app/components/infoDiabetesItems/medicamentosItem.js
@@ -25,7 +25,7 @@ const TipoDeInsulinaItem = () => {
           <TouchableOpacity
             style={styles.btnAdd}
             onPress={() => {
-              router.navigate("caminho-do-formulário");
+              router.navigate("../../screens/registerPressArterial");
             }}
           >
             <Text style={styles.btnText}>Adicionar medicamento</Text>
