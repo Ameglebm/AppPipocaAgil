e5df5c397f46ece4815477ed3e5b8f0d257f6b09
Warlley Rocha <warlleyrocha@icloud.com>
2025-01-15 17:19:48 -0300

    chore: removido tag não usada

--- meu-app/frontend/app/components/CustomHeader.js
+++ meu-app/frontend/app/components/CustomHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import BackIconSvg from "./svgComponenets/BackIconSvg";
 
