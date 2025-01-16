1babc505b706f3a7c8613e4e151b017dd11aad20
Warlley Rocha <warlleyrocha@icloud.com>
2025-01-16 18:14:10 -0300

    debug: instalar eslint e prettier novamente, que não estavam respondendo

--- meu-app/frontend/eslint.config.mjs
+++ meu-app/frontend/eslint.config.mjs
@@ -1,20 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import prettierConfig from "eslint-config-prettier"; // Correção aqui
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { languageOptions: { globals: globals.node } },
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {
-    settings: {
-      react: {
-        version: "18.2",
-      },
-    },
-  },
-  prettierConfig, // Aqui a configuração do Prettier
 ];
