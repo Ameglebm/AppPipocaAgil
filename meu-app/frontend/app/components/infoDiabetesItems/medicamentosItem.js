6092d84faeb4b7d5c2ebefc603cf4a623e4984f5
Warlley Rocha <warlleyrocha@icloud.com>
2025-02-12 14:15:51 -0300

    style: estilizar campos de medicamentos adicionados

--- meu-app/frontend/app/components/infoDiabetesItems/medicamentosItem.js
+++ meu-app/frontend/app/components/infoDiabetesItems/medicamentosItem.js
@@ -1,43 +1,48 @@
+// Bibliotecas externas
 import React from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Image,
-} from "react-native";
-import { useRouter, useLocalSearchParams } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { useSelector, useDispatch } from "react-redux";
+
+// Redux
 import {
   removeMedication,
   resetMedication,
 } from "../../reducers/medicationActions";
 
+// Dados
 import data from "../slidesInfoDiabetes"; // Importa o array com os dados para o carrossel
-import ButtonSave from "../ButtonSave";
+
+// Componentes
 import Button from "../Button";
-import Trash from "../SvgComponents/Trash";
+import ButtonSave from "../ButtonSave";
 import Edit from "../SvgComponents/Edit";
+import Trash from "../SvgComponents/Trash";
+
+// Assets
 import plusImage from "../../assets/images/plus.png";
 
 const MedicamentoItem = () => {
-  const medicamentosItem = data.find((item) => item.id === "4"); // Busca o item com id === '4' no array de dados
-
-  const router = useRouter();
-  const params = useLocalSearchParams();
+  // Obtém depêndencias
   const dispatch = useDispatch();
+  const params = useLocalSearchParams();
+  const router = useRouter();
 
+  // Busca e estados globais
+  const medicamentosItem = data.find((item) => item.id === "4"); // Busca o item com id === '4' no array de dados
   const medicamentos = useSelector(
     (state) => state.medication.medicamentos || []
   ); // Obtém o estado do Redux
+
   console.log("Medicamentos armazenados no Redux", medicamentos); // Para depuração
 
+  // Obtém o último medicamento adicionado
   const ultimoMedicamento =
     medicamentos.find((med) => med.id === params?.id) ||
     medicamentos[medicamentos.length - 1] ||
     null;
 
+  // Funções de manipulação de dados
   const handleSave = () => {
     console.log("salvo");
   };
@@ -78,41 +83,39 @@ const MedicamentoItem = () => {
               <Text style={styles.title}>Medicamentos</Text>
             </View>
 
-            <ScrollView style={styles.test}>
-              {medicamentos.map((medicamento) => {
-                return (
+            {medicamentos.map((medicamento) => {
+              return (
+                <View
+                  key={medicamento.id}
+                  style={styles.configuredContainerMedication}
+                >
                   <View
-                    key={medicamento.id}
-                    style={styles.configuredContainerMedication}
+                    style={{
+                      flexDirection: "row",
+                      gap: 8,
+                      alignItems: "center",
+                    }}
                   >
-                    <View
-                      style={{
-                        flexDirection: "row",
-                        gap: 8,
-                        alignItems: "center",
-                      }}
-                    >
-                      <Text style={styles.nomeMedicamento}>
-                        {medicamento.name}
-                      </Text>
-                      <Text style={styles.useText}>Uso contínuo</Text>
-                    </View>
-
-                    <View style={styles.containerEditDel}>
-                      <TouchableOpacity onPress={editMedication}>
-                        <Edit />
-                      </TouchableOpacity>
-
-                      <TouchableOpacity onPress={deleteMedication}>
-                        <Trash />
-                      </TouchableOpacity>
-                    </View>
+                    <Text style={styles.nomeMedicamento}>
+                      {medicamento.name}
+                    </Text>
+                    <Text style={styles.useText}>Uso contínuo</Text>
                   </View>
-                );
-              })}
-            </ScrollView>
 
-            <View style={{ alignItems: "center" }}>
+                  <View style={styles.containerEditDel}>
+                    <TouchableOpacity onPress={editMedication}>
+                      <Edit />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={deleteMedication}>
+                      <Trash />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              );
+            })}
+
+            <View style={styles.contentBtnFixed}>
               <TouchableOpacity
                 style={styles.btnAddMedication}
                 onPress={() => {
@@ -158,20 +161,11 @@ const MedicamentoItem = () => {
 export default MedicamentoItem;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#EDF3FF",
-    borderRadius: 16,
-    paddingTop: 24,
-    paddingHorizontal: 16,
-    paddingBottom: 18,
-    marginBottom: 232,
-  },
-  test: { height: 200 },
-  header: {
+  configuredHeader: {
     flexDirection: "column",
     alignItems: "flex-start",
     alignSelf: "stretch",
-    paddingBottom: 20,
+    padding: 16,
   },
   title: {
     color: "#282828",
@@ -180,6 +174,61 @@ const styles = StyleSheet.create({
     fontSize: 20,
     lineHeight: 22,
   },
+  configuredContainerMedication: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#EDF3FF",
+    padding: 16,
+    borderRadius: 16,
+    marginBottom: 10,
+  },
+  nomeMedicamento: {
+    color: "#282828",
+    fontFamily: "Urbanist_700Bold",
+    fontSize: 18,
+    lineHeight: 19.8,
+  },
+  useText: {
+    fontFamily: "Lato_400Regular",
+    fontSize: 12,
+  },
+  containerEditDel: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+    paddingRight: 5,
+  },
+  contentBtnFixed: {
+    position: "absolute",
+    top: 330,
+    left: 16,
+  },
+  btnAddMedication: {
+    width: 320,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textBtnAddMedication: {
+    color: "#5E5D5C",
+    fontFamily: "Urbanist_700Bold",
+    fontSize: 18,
+  },
+  container: {
+    backgroundColor: "#EDF3FF",
+    borderRadius: 16,
+    paddingTop: 24,
+    paddingHorizontal: 16,
+    paddingBottom: 18,
+    marginBottom: 232,
+  },
+  header: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    alignSelf: "stretch",
+    paddingBottom: 20,
+  },
+
   contentBtn: {
     backgroundColor: "#FDFDFD",
     width: 320,
@@ -198,45 +247,5 @@ const styles = StyleSheet.create({
     fontSize: 14,
     lineHeight: 21,
     color: "#282828",
-  },
-  nomeMedicamento: {
-    color: "#282828",
-    fontFamily: "Urbanist_700Bold",
-    fontSize: 18,
-    lineHeight: 19.8,
-  },
-  configuredHeader: {
-    flexDirection: "column",
-    alignItems: "flex-start",
-    alignSelf: "stretch",
-    padding: 16,
-  },
-  configuredContainerMedication: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "#EDF3FF",
-    padding: 16,
-    borderRadius: 16,
-  },
-  useText: {
-    fontFamily: "Lato_400Regular",
-    fontSize: 12,
-  },
-  btnAddMedication: {
-    width: 320,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  textBtnAddMedication: {
-    color: "#5E5D5C",
-    fontFamily: "Urbanist_700Bold",
-    fontSize: 18,
-  },
-  containerEditDel: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 16,
-    paddingRight: 5,
   },
 });
