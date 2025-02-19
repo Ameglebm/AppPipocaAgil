02b0bab123d317fd5b2cc22d149ae2a33d07fde8
Warlley Rocha <warlleyrocha@icloud.com>
2025-02-19 14:49:27 -0300

    fix: corrigir o valor do campo que não estava sendo atualizado ao selecionar um item da lista e value incorreto no label: depois do jantar. feature: criar useEffect para controlar o estado do botão de salvar e criar estado para gerenciar o tipo de tratamento selecionado. style: ajustar espaçamentos da screen. chore: adicionar comentários sobre o que falta ser desenvolvido

--- meu-app/frontend/app/screens/registerGlucose.jsx
+++ meu-app/frontend/app/screens/registerGlucose.jsx
@@ -1,18 +1,40 @@
-import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
-import React, { useState } from "react";
+// Bibliotecas externas
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+
+// Componentes customizados
 import Header from "../components/CustomHeader";
 import Dropdown from "../components/DropDown";
 import CustomInput from "../components/CustomInput";
 import ButtonSave from "../components/ButtonSave";
-//import Button from "../components/Button";
 
 export default function registerGlucose() {
   const [glicose, setGlicose] = useState(0);
+  const [treatment, setTreatment] = useState("");
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  // Função para verificar se o botão deve estar desabilitado
+  useEffect(() => {
+    if (glicose && treatment) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [glicose, treatment]);
 
   return (
     <View style={{ backgroundColor: "#FDFDFD", flex: 1 }}>
-      <Header title={"Registrar glicemia"} />
-      <View style={{ backgroundColor: "#FDFDFD", padding: 20 }}>
+      <View style={styles.header}>
+        <Header title={"Registrar glicemia"} />
+      </View>
+
+      <View
+        style={{
+          backgroundColor: "#FDFDFD",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Dropdown
           items={[
             {
@@ -26,11 +48,16 @@ export default function registerGlucose() {
             { label: "Antes do almoço", value: "Antes do almoço" },
             { label: "Depois do almoço", value: "Depois do almoço" },
             { label: "Antes do jantar", value: "Antes do jantar" },
-            { label: "Depois do jantar", value: "Antes de dormir" },
+            { label: "Depois do jantar", value: "Depois do jantar" },
             { label: "Extra", value: "Extra" },
           ]}
           placeholder={"Selecione o tipo"}
           title={"Informe o tipo de glicemia"}
+          style={customDropdownStyle}
+          value={treatment}
+          onValueChange={(item) => {
+            setTreatment(item);
+          }}
         />
 
         <CustomInput
@@ -42,21 +69,34 @@ export default function registerGlucose() {
         />
 
         <View style={styles.viewBtns}>
+          {/* 
+          Chamar o modal de "Tem certeza que deseja cancelar o registro de glicemia?"
+          O front possui um componente para modal em "/components/ConfirmationModal.js" basta chamar ele.
+          */}
           <TouchableOpacity style={styles.btnCancel}>
             <Text style={styles.btnTextCancel}>Cancelar</Text>
           </TouchableOpacity>
 
-          <ButtonSave style={customButtonStyles} disabled={true} />
+          {/* 
+          Funcção de salvar:
+          Adicionar uma condição para verificar o nivel da glicemia. 
+          - Se for <=75 "Glicemia está baixa" (Criar o modal "Atenção! Sua glicemia está baixa.")
+          - Se for > 75 e < 120 "Glicemia está normal" (Criar o modal "Parabéns! Sua glicemia está dentro da meta.")
+          - Se for >= 120 "Glicemia está alta" (Criar o modal "Atenção! Sua glicemia está alta.")
+          */}
+          <ButtonSave style={customButtonStyles} disabled={isDisabled} />
         </View>
       </View>
     </View>
   );
 }
 
+// Sobrescrição de estilos
 const customButtonStyles = {
   container: {
     width: 154,
     heigth: 36,
+    paddingLeft: 5,
   },
   moveButton: {
     width: 154,
@@ -65,7 +105,17 @@ const customButtonStyles = {
   },
 };
 
+const customDropdownStyle = {
+  dropDownContainer: {
+    marginTop: 5,
+  },
+};
+
+// Estilos
 const styles = StyleSheet.create({
+  header: {
+    marginTop: 45,
+  },
   viewBtns: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -76,6 +126,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     paddingTop: 5,
+    paddingRight: 20,
   },
   btnTextCancel: {
     color: "#898887",
