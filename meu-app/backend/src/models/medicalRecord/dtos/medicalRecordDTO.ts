bf318f9666789afe8d3ef4d1fa405aeca996be41
Gabriel Pinto <gabrielhgpinto@gmail.com>
2025-02-18 21:53:13 -0300

    Ajuste do DTO

--- meu-app/backend/src/models/medicalRecord/dtos/medicalRecordDTO.ts
+++ meu-app/backend/src/models/medicalRecord/dtos/medicalRecordDTO.ts
@@ -1,27 +1,40 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateDiabetesDTO {
-  @ApiProperty({ example: '123', description: 'ID do usuário' })
+  @ApiProperty({ example: 'string', description: 'ID do usuário' })
   @IsString({ message: 'O ID do usuário deve ser uma string.' })
   userId!: string;
 
-  @ApiProperty({ example: '1', description: 'ID do tipo de diabetes' })
+  @ApiProperty({ example: 'string', description: 'ID do tipo de diabetes' })
   @IsString({ message: 'O ID do tipo de diabetes deve ser uma string.' })
   diabetesId!: string;
 }
 
 export class GetDiabetesDTO {
-  @ApiProperty({ example: '123', description: 'ID do usuário' })
+  @ApiProperty({ example: 'string', description: 'ID do usuário' })
   @IsString({ message: 'O ID do usuário deve ser uma string.' })
   userId!: string;
 }
 
-// Tenho que incluir o isAtual aqui?
 export class PostLeituraGlicemiaDTO {
+  @ApiProperty({ example: 'number', description: 'ID do usuário' })
+  @IsNumber({ }, { message: 'ID do usuário deve ser um número' })
   userId!: number;
+  
+  @ApiProperty({ example: 'number', description: 'ID do período' })
+  @IsNumber({ }, { message: 'Período deve ser um número' })
   periodoId!: number;
+  
+  @ApiProperty({ example: 'number', description: 'Meta mínima' })
+  @IsNumber({ }, { message: 'Meta mínima deve ser um número' })
   metaMin!: number;
+  
+  @ApiProperty({ example: 'number', description: 'Meta ideal' })
+  @IsNumber({ }, { message: 'Meta ideal deve ser um número' })
   metaIdeal!: number;
+  
+  @ApiProperty({ example: 'number', description: 'Meta máxima' })
+  @IsNumber({ }, { message: 'Meta máxima deve ser um número' })
   metaMax!: number;
 }
