ce98935c8b3735c99dabcdb726914120116f8243
Ameglebm <amegleeu@gmail.com>
2025-03-12 12:57:06 -0300

    Ajustado DTos

--- meu-app/backend/src/models/userInsulin/dtos/userInsulinDTO.ts
+++ meu-app/backend/src/models/userInsulin/dtos/userInsulinDTO.ts
@@ -27,12 +27,25 @@ export class GetUserInsulinDTO{
 }
 
 export class PatchUserInsulinDTO{
-    @ApiProperty({example: '1', description: 'ID da insulina'})
+    @ApiProperty({ example: '1', description: 'ID da insulina'})
+    @IsNumber({}, {message: 'O ID deve ser um numero'})
+    id!: number
+
+    @ApiProperty({example: '1', description: 'ID do usuario'})
     @IsNumber({}, {message: 'O ID deve ser um numero'})
     userId!: number
+
+    @ApiProperty({ example: 'Dosagem', description: 'dosagem da insulina'})
+    @IsNumber({}, {message: 'A dosagem deve ser um numero'})
+    dosagemQtd!: number
+
 }
 
 export class DeleteUserInsulinDTO{
+    @ApiProperty({ example: '1', description: 'ID da insulina'})
+    @IsString({ message: 'O ID deve ser uma string'})
+    id!: string
+
     @ApiProperty({example: '1', description: 'ID do usuário'})
     @IsString({message: 'O ID do usuário deve ser uma string'})
     userId!: string
