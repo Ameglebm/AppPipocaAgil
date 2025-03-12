2b0a9f0c2e75634768f77a406ae3a114f076777d
Ameglebm <amegleeu@gmail.com>
2025-03-12 12:46:28 -0300

    UserInsulin.controller finalizado, finalizado(aguardando verificação e testes)

--- meu-app/backend/src/models/userInsulin/controller/userinsulin.controller.ts
+++ meu-app/backend/src/models/userInsulin/controller/userinsulin.controller.ts
@@ -1,11 +1,92 @@
 import { AuthGuard } from '@/middlewares/auth.guard';
-import { Controller, Post, Get, Patch, Delete, UseGuards, Inject } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Post, Get, Patch, Delete, UseGuards, Inject, Body, InternalServerErrorException, Param, NotFoundException } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { IUserInsulinService } from '../interface/userInsulinService.interface';
+import { CreateUserInsulinDTO, DeleteUserInsulinDTO, GetUserInsulinDTO, PatchUserInsulinDTO } from '../dtos/userInsulinDTO';
 
 @UseGuards(AuthGuard)
 @ApiTags('userInsulin')
 @Controller('userInsulin')
 
 export class UserinsulinController { 
-    constructor (@Inject('IUserInsulinService') private readonly userInsulinService: IUserInsulinService) {} }
+    constructor (@Inject('IUserInsulinService') private readonly userInsulinService: IUserInsulinService) {} 
+
+    @ApiOperation({ summary: 'Registrar administração de insulina'})
+    @ApiResponse({ status: 201, description: 'Administração de insulina registrada com sucesso'})
+    @ApiResponse({ status: 400, description: 'Erro de validação'})
+    @ApiResponse({ status: 500, description: 'Erro interno no servidor'})
+    @Post('insulin')
+    async createUserInsulin(@Body() insulinDto: CreateUserInsulinDTO): Promise<void> {
+        try {
+            await this.userInsulinService.createUserInsulin(insulinDto);
+        } catch (error) {
+            console.error('Erro ao registrar administração de insulina:', error);
+            throw new InternalServerErrorException('Erro interno no servidor');
+        }
+    }
+
+    @ApiOperation({ summary: 'Ober registro de insulina'})
+    @ApiResponse({ status: 200, description: 'Registro de insulina encontrado'})
+    @ApiResponse({ status: 400, description: 'Erro de validação' })
+    @ApiResponse({ status: 404, description: 'Registro de insulina não encontrado'})
+    @ApiResponse({ status: 500, description: 'Erro interno do servidor'})
+    @Get('insulina/:id')
+    async getUserInsulin(@Param() params: GetUserInsulinDTO) {
+        const record = await this.userInsulinService.getUserInsulin
+        (params)
+
+        return {data: record}
+
+    } catch (error: { message: string; }) {
+        if (error instanceof Error && error.message === 'Registro de insulua não encontrado') {
+            throw new NotFoundException(error.message)
+        }
+
+        console.error('Erro ao obter registro de insulina', error)
+        throw new InternalServerErrorException(
+            'Erro interno do servidor'
+        )}
+
+    @ApiOperation({ summary: 'Atualizar registro de insulina'})
+    @ApiResponse({ status: 200, description: 'Registro de insulina atualizado com sucesso'})
+    @ApiResponse({ status: 400, description: 'Erro de validação'})
+    @ApiResponse({ status: 500, description: 'Erro interno do servidor'})
+    @Patch('insulina/:id')
+    async patchUserInsulin(@Param() params: PatchUserInsulinDTO) {
+        try {
+            const record = await this.userInsulinService.patchUserInsulin(params)
+
+            return {data: record}
+        } catch (error) {
+            if (error instanceof Error && error.message === 'Erro ao atualizar registro de insulina') {
+                throw new NotFoundException('Erro interno do servidor')
+                }
+            }
+         }
+
+         @ApiOperation({ summary: 'Deletar registro de insulina'})
+         @ApiResponse({ status: 200, description: 'Registro de insulina deletado com sucesso'})
+         @ApiResponse({ status: 400, description: 'Erro de validação'})
+         @ApiResponse({ status: 404, description: 'Registro de insulina não encontrado'})
+         @ApiResponse({ status: 500, description: 'Erro interno do servidor'})
+         @Delete('insulina/:id')
+         async deleteUserInsulin(@Param() params: DeleteUserInsulinDTO){
+            try {
+                const record = await this.userInsulinService.deleteUserInsulin(params)
+
+                return {data: record}
+
+            } catch (error) {
+                if (error instanceof Error && error.message === 'Registro de insulina não encontrado') {
+                    throw new NotFoundException(error.message)
+                }
+
+                console.error('Erro ao deletar registro de insulina', error)
+                throw new InternalServerErrorException (
+                    'Erro interno do servidor')
+            }
+         }
+    }
+    
+
+
