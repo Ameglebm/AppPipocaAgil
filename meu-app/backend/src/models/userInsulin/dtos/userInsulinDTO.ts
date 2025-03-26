70660c2510ad54cc515fe15a8975f9a6a3c18e06
Ameglebm <amegleeu@gmail.com>
2025-03-25 22:46:14 -0300

    feat - DTO userInsulin

--- meu-app/backend/src/models/userInsulin/dtos/userInsulinDTO.ts
+++ meu-app/backend/src/models/userInsulin/dtos/userInsulinDTO.ts
@@ -1,28 +1,50 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNumber, IsNotEmpty, IsEnum, isString } from 'class-validator';
-
-type TipoUnidadeInsulina = 'mg ou algum q elas mandarem';
+import { IsString, IsNumber, IsNotEmpty, } from 'class-validator';
 
 export class CreateUserInsulinDTO{
     @ApiProperty({ example: '1', description: 'ID do usuario' })
     @IsNumber( {}, {message: 'O ID do usuario deve ser number.' })
-    userID!: number
+    userId!: number
 
     @ApiProperty({example: 'Insulina', description: 'Nome da insulina'})
-    @IsString()
+    @IsString({ message: 'O nome da insulina deve ser uma string' })
     @IsNotEmpty()
-    nomeInsulina!: string
+    insulina!: string
 
-    @ApiProperty({example: 'unidade de medida', description: 'dosagem da insulina'})
-    @IsString()
+    @ApiProperty({example: 'Dosagem', description: 'dosagem da insulina'})
+    @IsNumber({}, {message: 'A dosagem deve ser um numero'})
     @IsNotEmpty()
-    @IsEnum(['mg ou algum ali q elas passarem'])
-    tipoUnidadesInsulina!: TipoUnidadeInsulina
+    dosagemQtd!: number
 }
 
+// Ajustado para uma string
 export class GetUserInsulinDTO{
     @ApiProperty({example: '1', description: 'ID do usuario'})
-    @IsNumber({}, {message: 'O ID deve ser um numero'})
-    id!: number
+    @IsString({ message: 'O ID do usuario deve ser uma string'})
+    id!: string
 }
 
+export class PatchUserInsulinDTO{
+    @ApiProperty({ example: '1', description: 'ID do registro'})
+    @IsNumber({}, {message: 'O ID do registro deve ser um numero'}) 
+    id!: number
+
+    @ApiProperty({ example: '1', description: 'ID do usuario'})
+    @IsNumber({}, {message: 'O ID do usuario deve ser um numero'}) 
+    userId!: number
+
+    @ApiProperty({ example: 'Dosagem', description: 'Dosagem da insulina'})
+    @IsNumber({}, {message: 'A dosagem deve ser um numero'})
+    dosagemQtd!: number
+
+}
+
+export class DeleteUserInsulinDTO{
+    @ApiProperty({ example: '1', description: 'ID da insulina'})
+    @IsString({ message: 'O ID deve ser uma string'})
+    id!: string
+
+    @ApiProperty({example: '1', description: 'ID do usuário'})
+    @IsString({message: 'O ID do usuário deve ser uma string'})
+    userId!: string
+}
