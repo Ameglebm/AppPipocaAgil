645381f66f538755e69b242b2eef15540ac9e316
Ameglebm <amegleeu@gmail.com>
2025-03-27 21:19:34 -0300

    feat - table - UserPressaoArterial - feat - table - UserPeso

---
+++ meu-app/backend/prisma/migrations/20250327231635_feat_table_userpeso_feat_pressao_arterial/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "User_Pressao_Arterial" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "sistolica" INTEGER NOT NULL,
+    "diastolica" INTEGER NOT NULL,
+    "date" TEXT NOT NULL,
+    "time" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_Pressao_Arterial_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User_peso" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "peso" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_peso_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "User_Pressao_Arterial" ADD CONSTRAINT "User_Pressao_Arterial_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User_peso" ADD CONSTRAINT "User_peso_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
