8f96cdd25582340b1180e4626271ff8704ae8969
warlleyrocha <warlleyrocha@icloud.com>
2025-04-17 08:21:01 -0300

    alterar email mockado

--- meu-app/frontend/app/screens/Auth/loginScreen.jsx
+++ meu-app/frontend/app/screens/Auth/loginScreen.jsx
@@ -20,7 +20,7 @@ import { useDispatch } from "react-redux";
 import { setUserId, setUserName } from "../../reducers/authActions";
 
 export default function Login() {
-  const [email, setEmail] = useState("warlleyrocha@icloud.com");
+  const [email, setEmail] = useState("warlleyrocha1@gmail.com");
   const [password, setPassword] = useState("1601Wr20@");
   const [errorEmail, setErrorEmail] = useState(null);
   const [errorPassword, setErrorPassword] = useState(null);
