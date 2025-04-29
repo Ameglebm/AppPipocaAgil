1cc26c5bd06d7e6b8581d26b8141996a38c6411f
Warlley Rocha <warlleyrocha@icloud.com>
2025-04-29 12:10:30 -0300

    feat: remover payload e importar token

--- meu-app/frontend/app/reducers/insulinActions.js
+++ meu-app/frontend/app/reducers/insulinActions.js
@@ -1,41 +1,56 @@
 // actions/insulinActions.js
 
 import api from "../services/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const pushInsulin = (insulin, userId) => async (dispatch) => {
   try {
+    const token = await AsyncStorage.getItem("authToken");
+
     const requestBody = {
       userId: Number(userId),
       insulina: insulin.name,
       dosagemQtd: Number (insulin.dosage),
     };
 
-    const payload = {
-      id: insulin.id,               // Garante que o ID seja salvo
-      name: insulin.name,           // Salva o nome para exibir
-      unity: insulin.unity,         // Salva a unidade selecionada
-      dosage: insulin.dosage,}
-
     console.log("Corpo da requisição (POST):", requestBody);
 
-    const response = await api.post("/insulin/register", requestBody);
-    console.log("Requisição enviada:", response);
+    const response = await api.post("/insulin/register", requestBody, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
 
     dispatch({
       type: "PUSH_INSULIN",
-      payload: { ...requestBody, payload, timestamp: new Date().toISOString() },
+      payload: { ...requestBody, timestamp: new Date().toISOString() },
     });
-  } catch (error) {
-    if (error.response?.status === 400) {
-      console.error("Erro de validação: Verifique os dados enviados.");
-    } else if (error.response?.status === 500) {
-      console.error("Erro interno do servidor. Tente novamente mais tarde.");
-    } else {
+
+  switch (response.status) {
+        case 201:
+          console.log("Insulina do usuário registrado com sucesso!", response.data);
+          
+          break;
+
+        case 400:
+          console.error("Erro de validação! Verifique os dados enviados.");
+          break;
+
+        case 500:
+          console.error("Erro interno do servidor");
+          break;
+
+        default:
+          console.error("Resposta inesperada do servidor", response.status);
+          break;
+      }
+    } catch (error) {
       console.error(
-        `Erro inesperado: ${error.response?.status || "Desconhecido"} - ${error.response?.data?.message || "Erro desconhecido"}`
+        "Erro na requisição:",
+        error.response?.data || error.message
       );
     }
-  }
 };
 
 export const updateInsulinField = (id, updatedData, userId) => async (dispatch) => {
