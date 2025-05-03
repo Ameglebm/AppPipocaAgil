559f81d4ca00bebc9690e66459766081e664dee2
Warlley Rocha <warlleyrocha@icloud.com>
2025-05-03 02:14:13 -0300

    feat: puxar dados da requisição e exibir na lista

--- meu-app/frontend/app/screens/HomeTopBar/screenHistorico.jsx
+++ meu-app/frontend/app/screens/HomeTopBar/screenHistorico.jsx
@@ -1,114 +1,133 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { View, StyleSheet, Image, Text } from "react-native";
+import { useSelector, useDispatch  } from "react-redux";
+import { View, StyleSheet, Image, Text, ScrollView } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 
 // Componentes
 import Filters from "../../components/filters/Filters";
-import HealthRecordList from "../../components/HealthRecordList";
+
+//Redux
+import { fetchGlucose, fetchBloodPressure, fetchWeight } from "../../reducers/healthActions";
 
 // Assets
 import IconHistory from "../../assets/images/icons/history.png";
 
 export default function ScreenHistory() {
+  const dispatch = useDispatch();
+
+  const userId = useSelector((state) => state.auth.userId);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      if (userId) {
+        dispatch(fetchBloodPressure(userId));
+        dispatch(fetchGlucose(userId));
+        dispatch(fetchWeight(userId));
+      }
+    }, [userId])
+  );
+
   const glucoseRecords =
     useSelector((state) => state.health.glucoseRecords) || [];
 
   const weightRecords =
     useSelector((state) => state.health.weightRecords) || [];
 
-      // Obtendo os filtros do Redux
-  const { selectedHealthParams, selectedTimePeriod } = useSelector(
-    (state) => state.filter || {}
-  );
+  const bloodPressureRecords =
+    useSelector((state) => state.health.bloodPressureRecords) || [];
 
+  const generateTitle = (item) => {
+    switch (item.type) {
+      case "Glucose":
+        return "Glicemia";
+      case "Blood Pressure":
+        return "Pressão Arterial";
+      case "Weight":
+        return "Peso";
+      default:
+        return "Registro";
+    }
+  };
   
-  // Função para filtrar os registros com base nos filtros aplicados
-  const filterRecords = (records) => {
-    return records.filter((item) => {
-      // Filtrando pelos parâmetros de saúde
-      const matchesHealthParams = selectedHealthParams
-        ? selectedHealthParams.includes(item.healthParam)
-        : true; // Se não houver filtros, retorna true (não filtra)
+    // Função para renderizar cada item
+  const renderItem = (item) => {
+    const date = new Date(item.createdAt);
+    const day = date.getDate(); // Pega o dia da data
 
-      // Filtrando pelo período de tempo
-      const itemDate = new Date(item.timestamp);
-      const today = new Date();
-      let matchesTimePeriod = false;
+    const title = generateTitle(item); // Use o título do item (se disponível)
 
-      if (selectedTimePeriod === "ultimaSemana") {
-        const lastWeek = new Date(today.setDate(today.getDate() - 7));
-        matchesTimePeriod = itemDate >= lastWeek;
-      } else if (selectedTimePeriod === "ultimos15dias") {
-        const last15Days = new Date(today.setDate(today.getDate() - 15));
-        matchesTimePeriod = itemDate >= last15Days;
-      } else if (selectedTimePeriod === "ultimos30dias") {
-        const last30Days = new Date(today.setDate(today.getDate() - 30));
-        matchesTimePeriod = itemDate >= last30Days;
-      } else {
-        matchesTimePeriod = true; // Se não houver período de tempo selecionado, não filtra
-      }
+    let value = "";
 
-      // Retorna o registro se ele corresponder aos filtros
-      return matchesHealthParams && matchesTimePeriod;
-    });
+    if (item.type == "Glucose") {
+      value = item.value + " mg/dL";
+    } else if (item.type == "Blood Pressure") {
+      value = `${item.sistolica} / ${item.diastolica} mmHg`;
+    } else if (item.type == "Weight") {
+      value = item.peso + " kg";
+    }
+
+    return (
+      <View style={styles.recordItem}>
+        <View style={styles.recordContent}>
+          <Text style={styles.dateText}>{day} |</Text>
+          <Text style={styles.recordText}>{title}</Text>
+        </View>
+        <Text style={styles.textValue}>
+          {value}
+        </Text>
+      </View>
+    );
   };
 
-  // Filtrando os registros de glicemia e peso
-  const filteredGlucoseRecords = filterRecords(glucoseRecords);
-  const filteredWeightRecords = filterRecords(weightRecords);
-
-  const allRecords = [...glucoseRecords, ...weightRecords].sort(
-    (a, b) => new Date(a.timestamp) - new Date(b.timestamp)
-  );
-
-  const firstRecord = allRecords.length > 0 ? allRecords[0] : null;
-  
-
-  const monthNames = [
-    "JAN",
-    "FEV",
-    "MAR",
-    "ABR",
-    "MAI",
-    "JUN",
-    "JUL",
-    "AGO",
-    "SET",
-    "OUT",
-    "NOV",
-    "DEZ",
+  // Combine os registros de todas as fontes (glucose, weight, bloodPressure)
+  const combinedRecords = [
+    ...glucoseRecords.map((item) => ({ ...item, type: "Glucose" })),
+    ...bloodPressureRecords.map((item) => ({ ...item, type: "Blood Pressure" })),
+    ...weightRecords.map((item) => ({ ...item, type: "Weight" })),
   ];
 
-  const firstRecordMonth = firstRecord
-    ? monthNames[new Date(firstRecord.timestamp).getMonth()]
-    : "N/A"; // Se não houver registros
+   // Função para formatar mês e ano
+   const formatMonthYear = (dateStr) => {
+    const date = new Date(dateStr);
+    return date
+    .toLocaleString("pt-BR", {
+      month: "short",
+    })
+    .replace(".", "")
+    .toUpperCase();
+  };
+
+  // Agrupar por mês
+  const groupedByMonth = combinedRecords.reduce((acc, item) => {
+    const month = formatMonthYear(item.createdAt);
+    if (!acc[month]) acc[month] = [];
+    acc[month].push(item);
+    return acc;
+  }, {});
+
 
   return (
     
     <View style={styles.screen}>
       <View style={{ alignItems: "flex-end" }}>
-        <Filters />
+        <Filters/>
       </View>
 
-      <View style={{ flexDirection: "row", gap: 12, marginTop: 14 }}>
-        <Image source={IconHistory} />
-        <Text style={{ color: "#7A98FF" }}>{firstRecordMonth}</Text>
-      </View>
-
-      <HealthRecordList
-        records={filteredGlucoseRecords}
-        title="Glicemia"
-        unit="mg/dL"
-      />
-
-      <HealthRecordList
-        records={filteredWeightRecords.map((record) => ({
-          ...record,
-          value: record.peso, // Mapeia "peso" para "value"
-        }))}
-        title="Peso"
-        unit="kg"
-      />
+      <ScrollView style={{ marginTop: 20 }}>
+        {Object.entries(groupedByMonth).map(([month, records]) => (
+          <View key={month}>
+            <View style={{ flexDirection: "row", gap: 12, marginTop: 14, alignItems: "center" }}>
+              <Image source={IconHistory} />
+              <Text style={{ color: "#7A98FF", fontSize: 16 }}>
+              {month.toUpperCase()}
+              </Text>
+            </View>
+            {records.map((item, index) => (
+              <View key={index}>{renderItem(item)}</View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
