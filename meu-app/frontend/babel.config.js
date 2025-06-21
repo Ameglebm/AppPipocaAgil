818c07466983507d27dd019c0ca0c465b98981f0
Ameglebm <amegleeu@gmail.com>
2025-06-21 20:45:25 -0300

    Atualizado

--- meu-app/frontend/babel.config.js
+++ meu-app/frontend/babel.config.js
@@ -1,6 +1,38 @@
-export default function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.json',
+          ],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@assets': './assets',
+          },
+        },
+      ],
+    ],
   };
-}
+};
